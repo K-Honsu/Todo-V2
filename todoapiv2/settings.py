302b1f186f65ad8ad1f0196c284f83519a3cf1d1
P-Horus <feranmia51@gmail.com>
2023-03-18 22:40:55 +0100

    fixed hashing

--- todoapiv2/settings.py
+++ todoapiv2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Todoapi',
         'USER': 'postgres',
-        'PASSWORD': 'Anjola123@',
+        'PASSWORD': os.environ.get('SECRET_KEY_PGADMIN'),
         'HOST': 'localhost'
     }
 }
